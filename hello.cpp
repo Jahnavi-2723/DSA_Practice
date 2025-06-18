539ad6dbc2d3dd45984133cf21e159fa1bedff5c
Jahnavi Guntuboina <146942627+Jahnavi-2723@users.noreply.github.com>
2025-06-18 20:31:43 +0530

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<< "Enter value A:";
+    cin>> a;
+    cout<< "Enter value B:";
+    cin>> b;
+    cout<< "Sum:"<<(a+b)<<endl;
+    return 0;
+}
